fd08d5f1b0405321743c4b09db3afadb871adbb9
UnyieldingSpirit <samveld396@gmail.com>
2025-04-06 15:55:26 +0500

    fix-build

--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -1,4 +1,3 @@
-export { default as OnboardingSlider } from './OnboardingSlider';
 export { default as PropertyCategories } from './PropertyCategories';
 export { default as SearchWFFilter } from './SearchWFFilter';
 export { default as SurveyBanner } from './SurveyBanner';
